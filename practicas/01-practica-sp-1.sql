2d1e56ccf8f4ff18e3aa3f720523ee34e299948c
Jose Luis Gallardo <gallardohjl2@gmail.com>
2024-07-05 13:36:35 -0600

    Manipulacion Avanzada: Se agrego un nuevo sp en practica

---
+++ practicas/01-practica-sp-1.sql
@@ -0,0 +1,18 @@
+-- Crear un sp que solicite un idde una categoria
+-- y devuelva el promedio de los precios de sus productos
+use Northwind
+GO
+
+Create or alter proc sp_solicitar_promedio_prod
+@catego int -- Parametro de entrada 
+AS
+begin
+select avg(UnitPrice) as 'Promedio precios de los productos'
+from products
+where CategoryID = @catego;
+end
+GO
+
+exec sp_solicitar_promedio_prod 2
+
+exec sp_solicitar_promedio_prod @catego = 5
